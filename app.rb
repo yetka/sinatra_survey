f09d92429536867e6cf3f0b1773a3fe3abf40ca3
Guest User <Guest@epicodus-20.local>
2017-12-12 14:33:47 -0800

    ass index.erb survey_edit.erb to views

--- app.rb
+++ app.rb
@@ -15,7 +15,31 @@ post('/survey_form') do
   name = params.fetch("name")
   survey = Survey.new({:name => name, :id => nil})
   survey.save()
-  @test = survey.capitalize_name
+  @surveys = Survey.all()
+  erb(:index)
+end
+
+get ('/surveys_list') do
+  @surveys = Survey.all()
+  erb(:surveys_list)
+end
+
+get ('/surveys/:id/edit') do
+  @survey = Survey.find(params.fetch("id").to_i())
+  erb(:survey_edit)
+end
+
+patch("/survey_edit/:id") do
+  name = params.fetch("name")
+  @survey = Survey.find(params.fetch("id").to_i())
+  @survey.update({:name => name})
+  @surveys = Survey.all()
+  erb(:index)
+end
+
+delete("/survey_edit/:id") do
+  @survey = Survey.find(params.fetch("id").to_i())
+  @survey.delete()
   @surveys = Survey.all()
   erb(:index)
 end
